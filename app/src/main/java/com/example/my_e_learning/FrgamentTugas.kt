02c648f7df0361cb61e9285041a8fcc888db4b82
alrizal <alrizalzul@gmail.com>
2024-01-30 01:50:56 +0700

    penambahan item dan fragment

--- app/src/main/java/com/example/my_e_learning/FrgamentTugas.kt
+++ app/src/main/java/com/example/my_e_learning/FrgamentTugas.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FramentUX.newInstance] factory method to
+ * Use the [FrgamentTugas.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FramentUX : Fragment() {
+class FrgamentTugas : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class FramentUX : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frament_u_x, container, false)
+        return inflater.inflate(R.layout.fragment_frgament_tugas, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class FramentUX : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FramentUX.
+         * @return A new instance of fragment FrgamentTugas.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FramentUX().apply {
+            FrgamentTugas().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
