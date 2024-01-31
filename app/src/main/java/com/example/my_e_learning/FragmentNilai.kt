8516365d0a6c93ed36f9aa412fb959afcdf18d5e
alrizal <alrizalzul@gmail.com>
2024-01-31 14:17:39 +0700

    fragment nilai

--- app/src/main/java/com/example/my_e_learning/FragmentNilai.kt
+++ app/src/main/java/com/example/my_e_learning/FragmentNilai.kt
@@ -1,30 +1,36 @@
+package com.example.my_e_learning
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.my_e_learning.databinding.FragmentHomeBinding
+import androidx.fragment.app.Fragment
 import com.example.my_e_learning.databinding.FragmentNilaiBinding
 
-private var _binding: FragmentNilaiBinding? = null
-private val binding get() = _binding!!
+class FragmentNilai : Fragment() {
 
+    private var _binding: FragmentNilaiBinding? = null
+    private val binding get() = _binding!!
 
-override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-): View? {
-    _binding = FragmentNilaiBinding.inflate(inflater, container, false)
-    return binding.root
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNilaiBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
 
-override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    initView ()
-}
-private fun initView (){
-}
 
-override fun onDestroyView() {
-    super.onDestroyView()
-    _binding = null
-}
